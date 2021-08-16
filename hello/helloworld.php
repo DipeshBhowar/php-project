8ed8c7171045db2f37c37b02fd7d3af2fa143901
dipesh <dipeshbhowar001@gmail.com>
2021-08-16 14:47:41 +0530

    create helloworld.php
    created new file helloworld.php

---
+++ hello/helloworld.php
@@ -0,0 +1,13 @@
+<?php 
+
+class basicmath{
+    
+    public static function add($a,$b)
+    {
+        # code...
+        $c = intval($a)+intval($b);
+        return intval($c);
+    }
+    
+}
+?>
